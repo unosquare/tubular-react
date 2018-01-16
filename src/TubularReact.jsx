a36d2ec07cf6a800aa96bb59808ad2efe3dcc517
Armando Cifuentes <armando.cifuentes@unosquare.com>
2018-01-16 10:10:21 -0600

    Base project

---
+++ src/TubularReact.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TubularReact extends Component{
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>);
+  }
+}
+
+export default TubularReact;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TubularReact from './TubularReact.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
