e6e6a2f47b69db67853cce984ab3d8be552489ba
Juan Alcala <juan.alcala@unosquare.com>
2018-01-16 16:29:19 -0600

    New component files

---
+++ src/TubularTable/TubularTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TubularTable extends React.Component{
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1>Hello World</h1>
+      </div>);
+  }
+}
+
+export default TubularTable;
