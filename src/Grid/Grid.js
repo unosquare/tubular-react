9e8048012940af7e95683cd04c4145c729dd919b
Juan Alcala <juan.alcala@unosquare.com>
2018-01-17 11:39:39 -0600

    Update base grid

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,12 +1,84 @@
+import Paper from 'material-ui/Paper';
+import PropTypes from 'prop-types';
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  }
+});
 
 class Grid extends React.Component{
+  static defaultProps = {
+    rowsPerPage: 5,
+    page: 0,
+    initialSort: {
+      column: '',
+      order: 'asc'
+    },
+    title: ''
+  }
+
+  state = {
+    order: this.props.initialSort.order,
+    orderBy: this.props.initialSort.column,
+    page: this.props.page,
+    rowsPerPage: this.props.rowsPerPage,
+    data: this.props.data
+  }
+
   render() {
+    const { classes, columns } = this.props;
+    const { data, rowsPerPage, page, order, orderBy } = this.state;
     return (
-      <div style={{ textAlign: 'center' }}>
-        <h1>Hello World</h1>
-      </div>);
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              { columns.map(column => 
+                <TableCell key={column.key}>
+                  {column.label}
+                </TableCell>
+              )}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, rowIndex) => (
+                <TableRow hover key={rowIndex}>
+                  { columns.map((column, index) => 
+                    <TableCell key={index} padding={column.label === '' ? 'none' : 'default'}>
+                      { row[column.key] && row[column.key] }
+                    </TableCell>
+                  ) }
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Paper>);
   }
 }
 
-export default Grid;
+Grid.propTypes = {
+  classes: PropTypes.object.isRequired,
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      sortable: PropTypes.bool.isRequired,
+      render: PropTypes.func
+    })).isRequired,
+  data: PropTypes.array.isRequired,
+  initialSort: PropTypes.shape({
+    column: PropTypes.string.isRequired,
+    order: PropTypes.string.isRequired
+  }),
+  rowsPerPage: PropTypes.number,    
+  title: PropTypes.string
+};
+export default withStyles(styles)(Grid);
