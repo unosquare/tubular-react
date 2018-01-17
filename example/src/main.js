9eee98a12ce2712f07efabc60c931a9209ba7498
Juan Alcala <juan.alcala@unosquare.com>
2018-01-17 15:47:24 -0600

    Update example using tubular model

--- example/src/main.js
+++ example/src/main.js
@@ -1,10 +1,22 @@
 import Grid from '../../src/Grid/Grid';
 import React from 'react';
 
+const columns = [
+  { 
+    name: 'key', 
+    label: 'label', 
+    sortable: true,
+    sortDirection: 'none',
+    searchable: true,
+    visible: true,
+    isKey: false,
+    dataType: 'string'
+  }
+];
 export default class Main extends React.Component{
   render(){
     return(
-      <Grid data={[{ key: 'data' }]} columns={[{ key: 'key', label: 'label', sortable: true }]}/>
+      <Grid data={[{ key: 'data' }]} columns={columns}/>
     );
   }
 }
