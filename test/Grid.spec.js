73ff8445bc98da339d58fa672a7c05be3097fb0f
Juan Alcala <juan.alcala@unosquare.com>
2018-01-17 11:39:55 -0600

    Update Grid test

--- test/Grid.spec.js
+++ test/Grid.spec.js
@@ -1,24 +1,52 @@
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
 import Grid from '../src/Grid/Grid';
+import Paper from 'material-ui/Paper';
 import PropTypes from 'prop-types';
 import React from 'react';
 import sinon from 'sinon';
 import Enzyme, { mount, shallow } from 'enzyme';
-
-
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Grid />', () => {
-  let wrapper;
+  let mountedGrid;
+  let props;
 
-  before( () => {
-    wrapper = shallow(<Grid/>);
-  });
+  const columns = [{
+    key: 'key',
+    label: 'col',
+    sortable: true
+  }];
+
+  const grid = () => {
+    if(!mountedGrid){
+      mountedGrid = mount(<Grid {...props} />);
+    }
+    return mountedGrid;
+  };
+
   
-  it('should render a div', () => {
-    const div = wrapper.find('div');
-    expect(div).to.not.be.null;
+  it('should render a Paper', () => {
+    const wrapper = grid().find(Paper);
+    expect(wrapper).to.have.lengthOf(1);
+  });
+
+  it('should have 1 column', () => {
+    const columns = grid().find(TableHead).find(TableRow).find(TableCell);
+    expect(columns).to.have.lengthOf(1);
+  });
+
+  it('should have 0 rows', () => {
+    const rows = grid().find(TableBody).find(TableRow);
+    expect(rows).to.have.lengthOf(0);
+  });
+
+  beforeEach(() => {
+    props = {
+      data: [],
+      columns: columns
+    };
   });
 });
