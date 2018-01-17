6569d72660fead762c2ba999617308b12fd0edfd
Juan Pablo Alcalá <32104554+jpalcala@users.noreply.github.com>
2018-01-17 16:44:12 -0600

    Use tubular column model (#21)
    * Update dependencies for examples
    
    * Add example
    
    * Ignore example bundle.js
    
    * Add watch task
    
    * Use tubular column model
    
    * Update example using tubular model
    
    * Add empty rows and remove unnecessary variables
    
    * Update babel-preset
    
    * Update tests
    
    * Update datatypes
    
    * Update datatypes again

---
+++ example/src/main.js
@@ -0,0 +1,32 @@
+import Grid from '../../src/Grid/Grid';
+import React from 'react';
+
+const columns = [
+  { 
+    name: 'key', 
+    label: 'label', 
+    sortable: true,
+    sortDirection: 'none',
+    searchable: true,
+    visible: true,
+    isKey: false,
+    dataType: 'string'
+  },
+  { 
+    name: 'sdasda', 
+    label: 'asdasda', 
+    sortable: true,
+    sortDirection: 'none',
+    searchable: true,
+    visible: true,
+    isKey: false,
+    dataType: 'string'
+  }
+];
+export default class Main extends React.Component{
+  render(){
+    return(
+      <Grid data={[{ key: 'data' }]} columns={columns}/>
+    );
+  }
+}
--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -17,31 +17,28 @@ class Grid extends React.Component{
   static defaultProps = {
     rowsPerPage: 5,
     page: 0,
-    initialSort: {
-      column: '',
-      order: 'asc'
-    },
     title: ''
   }
 
   state = {
-    order: this.props.initialSort.order,
-    orderBy: this.props.initialSort.column,
     page: this.props.page,
     rowsPerPage: this.props.rowsPerPage,
-    data: this.props.data
+    data: this.props.data,
+    columns: this.props.columns
   }
 
   render() {
-    const { classes, columns } = this.props;
-    const { data, rowsPerPage, page, order, orderBy } = this.state;
+    const { classes } = this.props;
+    const { data, rowsPerPage, page, columns } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
               { columns.map(column => 
-                <TableCell key={column.key}>
+                <TableCell key={column.name}>
                   {column.label}
                 </TableCell>
               )}
@@ -53,11 +50,16 @@ class Grid extends React.Component{
                 <TableRow hover key={rowIndex}>
                   { columns.map((column, index) => 
                     <TableCell key={index} padding={column.label === '' ? 'none' : 'default'}>
-                      { row[column.key] && row[column.key] }
+                      { row[column.name] && row[column.name] }
                     </TableCell>
                   ) }
                 </TableRow>
               ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 49 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </Paper>);
@@ -68,16 +70,18 @@ Grid.propTypes = {
   classes: PropTypes.object.isRequired,
   columns: PropTypes.arrayOf(
     PropTypes.shape({
-      key: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       sortable: PropTypes.bool.isRequired,
-      render: PropTypes.func
+      sortOrder: PropTypes.number,
+      sortDirection: PropTypes.oneOf(['none', 'asc', 'desc']).isRequired,
+      searchable: PropTypes.bool.isRequired,
+      visible: PropTypes.bool.isRequired,
+      isKey: PropTypes.bool.isRequired,
+      dataType: PropTypes.oneOf(['date', 'datetime', 'datetimeutc', 'numeric', 'boolean', 'string']).isRequired,
+      filter: PropTypes.bool.isRequired
     })).isRequired,
   data: PropTypes.array.isRequired,
-  initialSort: PropTypes.shape({
-    column: PropTypes.string.isRequired,
-    order: PropTypes.string.isRequired
-  }),
   rowsPerPage: PropTypes.number,    
   title: PropTypes.string
 };
