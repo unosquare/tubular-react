862f229d2cfb5ff1a94f61acf1c132f2dc46390a
Juan Alcala <juan.alcala@unosquare.com>
2018-01-18 12:11:22 -0600

    Add export menu and proptypes

--- src/Grid/GridToolbar.js
+++ src/Grid/GridToolbar.js
@@ -1,4 +1,6 @@
+import Button from 'material-ui/Button';
 import CloseIcon from 'material-ui-icons/Close';
+import DownloadIcon from 'material-ui-icons/FileDownload';
 import { FormControl } from 'material-ui/Form';
 import IconButton from 'material-ui/IconButton';
 import PrintIcon from 'material-ui-icons/Print';
@@ -8,7 +10,7 @@ import SearchIcon from 'material-ui-icons/Search';
 import Toolbar from 'material-ui/Toolbar';
 import { withStyles } from 'material-ui/styles';
 import Input, { InputAdornment } from 'material-ui/Input';
-
+import Menu, { MenuItem } from 'material-ui/Menu';
 
 const styles = theme => ({
   spacer: {
@@ -20,17 +22,28 @@ const styles = theme => ({
   formControl: {
     margin: theme.spacing.unit,
     minWidth: 250
+  },
+  button: {
+    minWidth: 150
+  },
+  paper: {
+    
   }
 });
 
 class GridToolbar extends React.Component {
   state = {
-    searchText: ''
+    searchText: '',
+    anchorEl: null
   }
 
-  handleInputChange = name => event => {
+
+  handleInputChange = event => {
     this.setState({
-      [name]: event.target.value
+      searchText: event.target.value
+    }, () => {
+      if(this.props.onSearchTextChange)
+        this.props.onSearchTextChange(this.state.searchText);
     });
   }
 
@@ -39,13 +52,38 @@ class GridToolbar extends React.Component {
       searchText: ''
     });
   }
+  
+  handleMenuOpen = event => {
+    this.setState({
+      anchorEl: event.currentTarget
+    });
+  }
+
+  handleMenuClose = () => {
+    this.setState({
+      anchorEl: null
+    });
+  }
+
 
   render(){
-    const { classes, isPrintEnabled } = this.props;
-    const { searchText } = this.state;
+    const { classes, isPrintEnabled, isExportEnabled } = this.props;
+    const { searchText, anchorEl } = this.state;
     return(
       <Toolbar>
         <div className={classes.spacer}></div>
+        {
+          isExportEnabled && 
+          <Button
+            raised
+            color='accent'
+            className={classes.button}
+            onClick={this.handleMenuOpen}
+          >
+            <DownloadIcon/>
+            Export csv
+          </Button>
+        }
         {
           isPrintEnabled && 
           <IconButton>
@@ -54,11 +92,12 @@ class GridToolbar extends React.Component {
         }
         <FormControl className={classes.formControl}>
           <Input
+            margin='none'
             fullWidth
             className={classes.searchField}
             type='text'
             value={this.state.searchText}
-            onChange={this.handleInputChange('searchText')}
+            onChange={this.handleInputChange}
             startAdornment={
               <InputAdornment position='end'>
                 <IconButton>
@@ -76,14 +115,24 @@ class GridToolbar extends React.Component {
             }
           />
         </FormControl>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleMenuClose}
+        >
+          <MenuItem onClick={this.handleMenuClose}>All rows</MenuItem>
+          <MenuItem onClick={this.handleMenuClose}>Current rows</MenuItem>
+        </Menu>
       </Toolbar>
     );
   }
 }
 
 GridToolbar.propTypes = {
-  classes: PropTypes.object.isRequired,
-  isPrintEnabled: PropTypes.bool.isRequired
+  classes: PropTypes.object.isRequired,  
+  isExportEnabled: PropTypes.bool,
+  isPrintEnabled: PropTypes.bool,
+  onSearchTextChange: PropTypes.func
 };
 
 export default withStyles(styles)(GridToolbar);
