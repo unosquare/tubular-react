f46be94602808c1d47b9784bd571ff6924f67ffb
Armando Cifuentes <armando.cifuentes@unosquare.com>
2018-01-22 12:54:49 -0600

    Adding validations on Axios response

--- src/Grid/RemoteDataSource.js
+++ src/Grid/RemoteDataSource.js
@@ -9,6 +9,7 @@ class RemoteDataSource {
     this.counter = 0;
     this.dataStream = new Rx.BehaviorSubject({ Payload: [] });
     this.columns = columns;
+    this.errorMessage = '';
   }
   
   connect(rowsPerPage, page) {
@@ -27,6 +28,28 @@ class RemoteDataSource {
   refresh(rowsPerPage, page) {
     this._doRequest(rowsPerPage, page);
   }
+
+  handleError(error) {
+    // TODO: Check what kind of error I'm receiving
+    console.log(error);
+  }
+
+  handleValidateStructure(objectGiven) {
+    const expectedStructure = {
+      'Counter': null,
+      'Payload': null,
+      'TotalRecordCount': null, 
+      'FilteredRecordCount': null,
+      'TotalPages': null,
+      'CurrentPage': null,
+      'AggregationPayload': null
+    };
+
+    const expectedStructureKeys = Object.keys(expectedStructure).sort();
+    const objectGivenKeys = Object.keys(objectGiven).sort();
+
+    return JSON.stringify(expectedStructureKeys) === JSON.stringify(objectGivenKeys);
+  }
   
   _doRequest(rowsPerPage, page) {
     const request = {
@@ -39,20 +62,24 @@ class RemoteDataSource {
     };
 
     Axios.post(this.url, request).then(response => {
-      const data = response.data.Payload;
-      const rows = data.map(row => {
-        const obj = {};
-        
-        this.columns.forEach((column, key) => {
-          obj[column.Name] = row[key] || row[column.Name];
-        });
-        
-        return obj;
-      });
-    
-      this.dataStream.onNext({ Payload: rows });
-    }).catch(response => {
-      console.log(response);
+      if(response.data !== undefined) {
+        if(this.handleValidateStructure(response.data)) {
+          const data = response.data.Payload;
+          const rows = data.map(row => {
+            const obj = {};
+          
+            this.columns.forEach((column, key) => {
+              obj[column.Name] = row[key] || row[column.Name];
+            });
+          
+            return obj;
+          });
+      
+          this.dataStream.onNext({ Payload: rows });
+        }
+      }
+    }).catch(error => {
+      this.handleError(error);
     });
   }
 }
