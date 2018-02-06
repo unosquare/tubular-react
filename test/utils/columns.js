5858766b96b98d91a4bbfa5fd60a21a337e697c0
Armando Cifuentes <32103862+ArCiGoUnoSquare@users.noreply.github.com>
2018-02-06 12:41:49 -0600

    Issue54-Clean <Grid /> test file (#59)
    * Clean <Grid /> test file: The constants used in the tests were placed into files
    
    * Place valid and invalid tubular column structure in one file

---
+++ test/utils/columns.js
@@ -0,0 +1,68 @@
+const validColumnsSample = [
+  {
+    Label: 'Order ID',
+    Name: 'OrderID',
+    Sortable: true,
+    SortOrder: 1,
+    SortDirection: 'Descending',
+    IsKey: true,
+    DataType: 'numeric',    
+    Filtering: true
+  },
+  {
+    Label: 'Customer Name',
+    Name: 'CustomerName',
+    Searchable: true,
+    Aggregate: 'Count',
+    Filtering: true
+  },
+  {
+    Label: 'Shipped Date',
+    Name: 'ShippedDate',
+    DataType: 'datetime',
+    Filtering: true
+  },
+  {
+    Label: 'Shipper City',
+    Name: 'ShipperCity',
+  },
+  {
+    Label: 'Amount',
+    Name: 'Amount',
+    DataType: 'numeric'
+  }
+];
+
+const invalidColumnsSample = [
+  {
+    SortOrder: 1,
+    SortDirection: 'Descending',
+    IsKey: true,
+    DataType: 'numeric',    
+    Filtering: true
+  },
+  {
+    Label: 'Customer Name',
+    Name: 'CustomerName',
+    Searchable: true,
+    Aggregate: 'Count',
+    Filtering: true
+  },
+  {
+    Label: 'Shipped Date',
+    Name: 'ShippedDate',
+    DataType: 'datetime',
+    Filtering: true
+  },
+  {
+    Label: 'Shipper City',
+    Name: 'ShipperCity',
+  },
+  {
+    Label: 'Amount',
+    Name: 'Amount',
+    DataType: 'numeric'
+  }
+];
+
+export { validColumnsSample, invalidColumnsSample };
