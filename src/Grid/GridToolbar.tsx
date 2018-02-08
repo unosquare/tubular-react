8761c316025b125f97e8c3206f0cb5bace0eabb5
Juan Alcala <juan.alcala@unosquare.com>
2018-02-08 12:05:34 -0600

    Add tslint

--- src/Grid/GridToolbar.tsx
+++ src/Grid/GridToolbar.tsx
@@ -1,64 +1,66 @@
-import * as PropTypes from 'prop-types';
-import * as React from 'react';
+import { Button, WithStyles, withStyles } from 'material-ui';
 import CloseIcon from 'material-ui-icons/Close';
 import DownloadIcon from 'material-ui-icons/FileDownload';
-import { FormControl } from 'material-ui/Form';
-import IconButton from 'material-ui/IconButton';
 import PrintIcon from 'material-ui-icons/Print';
 import SearchIcon from 'material-ui-icons/Search';
-import Toolbar from 'material-ui/Toolbar';
-import { StyleRules, Theme } from 'material-ui/styles';
-import { WithStyles, Button, withStyles } from 'material-ui';
+import { FormControl } from 'material-ui/Form';
+import IconButton from 'material-ui/IconButton';
 import Input, { InputAdornment } from 'material-ui/Input';
 import Menu, { MenuItem } from 'material-ui/Menu';
+import { StyleRules, Theme } from 'material-ui/styles';
+import Toolbar from 'material-ui/Toolbar';
+import * as PropTypes from 'prop-types';
+import * as React from 'react';
+
 import { MouseEvent } from 'react';
 
 const styleClasses  = {
-  formControl:'',
-  spacer:'',
-  button:''
+  button: '',
+  formControl: '',
+  spacer: '',
 };
 
 const styles = (theme: Theme): StyleRules<keyof typeof styleClasses> => (
-  { 
+  {
+    button: {
+      minWidth: 150
+    },
     formControl: {
       margin: theme.spacing.unit,
       minWidth: 250
     },
     spacer: {
       flex: '1 1 100%'
-    },
-    button: {
-      minWidth: 150
-    },
+    }
   }
 );
 
-interface State {  
-  anchorEl?: HTMLElement,
-  searchText: string,
+interface IState {
+  anchorEl?: HTMLElement;
+  searchText: string;
 }
 
-interface Props {
-  gridName: string,
-  onSearchTextChange(text: string): void,
-  onPrint(): void,
-  onExport(condition:boolean):void
-  isExportEnabled: boolean,
-  isPrintEnabled: boolean,
-  filteredRecordCount: number
+interface IProps {
+  gridName: string;
+  isExportEnabled: boolean;
+  isPrintEnabled: boolean;
+  filteredRecordCount: number;
+  onSearchTextChange(text: string): void;
+  onPrint(): void;
+  onExport(condition: boolean): void;
 }
 
-class GridToolbar extends React.Component <Props & WithStyles<keyof typeof styleClasses>,State> {
-  static defaultProps= {
-    onSearchTextChange: (x:any):any => x
-  }
-  state = {
-    searchText: '',
-    anchorEl: null as HTMLElement
+class GridToolbar extends React.Component <IProps & WithStyles<keyof typeof styleClasses>, IState> {
+  public static defaultProps = {
+    onSearchTextChange: (x: any): any => x
   };
 
-  componentDidMount() {
+  public state = {
+    anchorEl: null as HTMLElement,
+    searchText: '',
+  };
+
+  public componentDidMount() {
     const searchText = localStorage.getItem(`tubular.${this.props.gridName}_searchText`);
 
     if (searchText) {
@@ -68,30 +70,30 @@ class GridToolbar extends React.Component <Props & WithStyles<keyof typeof style
     }
   }
 
-  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>):void => {
+  public handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const searchText = event.target.value;
     this.setState({ searchText }, () => this.props.onSearchTextChange(searchText));
   }
 
-  clearSearchText = ():void => {
+  public clearSearchText = (): void => {
     this.setState({
       searchText: ''
     }, () => this.props.onSearchTextChange(this.state.searchText));
   }
 
-  handleMenuOpen = (event: React.MouseEvent<HTMLElement>):void => {
+  public handleMenuOpen = (event: React.MouseEvent<HTMLElement>): void => {
     this.setState({
       anchorEl: event.currentTarget
     });
   }
 
-  handleMenuClose = ():void => {
+  public handleMenuClose = (): void => {
     this.setState({
       anchorEl: null as HTMLElement
     });
   }
-  
-  exportCSV = (filtered: boolean, e:React.MouseEvent<HTMLElement>) => {
+
+  public exportCSV = (filtered: boolean, e: React.MouseEvent<HTMLElement>) => {
     const { onExport } = this.props;
     e.preventDefault();
     this.setState({
@@ -100,12 +102,12 @@ class GridToolbar extends React.Component <Props & WithStyles<keyof typeof style
     onExport(filtered);
   }
 
-  render(){
-    const { classes, isPrintEnabled, isExportEnabled, onPrint, filteredRecordCount } = this.props;
+  public render() {
+    const { classes, filteredRecordCount, isPrintEnabled, isExportEnabled, onPrint} = this.props;
     const { searchText, anchorEl } = this.state;
     return(
       <Toolbar>
-        <div className={classes.spacer}></div>
+        <div className={classes.spacer}/>
         {
           isExportEnabled &&
           <IconButton disabled={filteredRecordCount === 0} onClick={this.handleMenuOpen}>
@@ -113,14 +115,14 @@ class GridToolbar extends React.Component <Props & WithStyles<keyof typeof style
           </IconButton>
         }
         {
-          isPrintEnabled && 
+          isPrintEnabled &&
           <IconButton onClick={onPrint} disabled={filteredRecordCount === 0}>
             <PrintIcon/>
           </IconButton>
         }
         <FormControl className={classes.formControl}>
           <Input
-            fullWidth
+            fullWidth={true}
             type='text'
             value={this.state.searchText}
             onChange={this.handleInputChange}
@@ -142,8 +144,8 @@ class GridToolbar extends React.Component <Props & WithStyles<keyof typeof style
           />
         </FormControl>
         <Menu anchorEl={anchorEl}  open={Boolean(anchorEl)} onClose={this.handleMenuClose}>
-          <MenuItem onClick={e => this.exportCSV(false, e)}> All rows</MenuItem>
-          <MenuItem onClick={e => this.exportCSV(true, e)}> Current rows</MenuItem>
+          <MenuItem onClick={(e) => this.exportCSV(false, e)}> All rows</MenuItem>
+          <MenuItem onClick={(e) => this.exportCSV(true, e)}> Current rows</MenuItem>
         </Menu>
       </Toolbar>
     );
--- src/Grid/RemoteDataSource.ts
+++ src/Grid/RemoteDataSource.ts
@@ -1,40 +1,119 @@
 import Axios from 'axios';
 import * as Rx from 'rx';
-class RemoteDataSource {
-  columns: Array<any>;
-  dataStream: any;
-  url: string;
-  counter: number;
 
-  static defaultColumnValues ={
-    Sortable: false,
-    Searchable: false,
+class RemoteDataSource {
+  public static defaultColumnValues = {
     Aggregate: 'None',
     DataType: 'string',
     IsKey: false,
+    Searchable: false,
+    Sortable: false,
     Visible: true
-  }
+  };
 
-  constructor(url : string, columns : Array<any>) {
+  public columns: any[];
+
+  public dataStream: any;
+
+  public url: string;
+
+  public counter: number;
+
+  constructor(url: string, columns: any[]) {
     this.url = url;
     this.counter = 0;
     this.dataStream = new Rx.BehaviorSubject({ payload: [] });
-    this.columns = this._normalizeColumns(columns);
+    this.columns = this.normalizeColumns(columns);
   }
 
-  connect(rowsPerPage: number, page: number, searchText: number) {
+  public connect(rowsPerPage: number, page: number, searchText: number) {
     this._updateDataStream(rowsPerPage, page, searchText);
     return this.dataStream;
   }
 
-  refresh(rowsPerPage: number, page: number, searchText: number){
+  public refresh(rowsPerPage: number, page: number, searchText: number){
     this._updateDataStream(rowsPerPage, page, searchText);
   }
 
-  _normalizeColumns= (columns: Array<any>) => 
-    columns.map(column => {
+  public getAllRecords = (rowsPerPage: number, page: number, searchText: number): Promise<object> =>
+  new Promise((resolve, reject) => {
+    const request = {
+      Columns: this.columns,
+      Count: this.counter++,
+      Search: { Text: searchText ? searchText : '', Operator: 'Auto' },
+      Skip: page * rowsPerPage,
+      Take: rowsPerPage,
+      TimezoneOffset: 360
+    };
+
+    Axios.post(this.url, request).then((response) => {
+      if (response.data === undefined || !this.isValidResponse(response.data)) {
+        throw new Error('It\'s not a valid Tubular response object');
+      }
+
+      const data = response.data.Payload;
+      const rows = data.map((row: any) => {
+        const obj: any = {};
+
+        this.columns.forEach((column: any, key: any) => {
+          obj[column.Name] = row[key] || row[column.Name];
+        });
+
+        return obj;
+      });
+
+      resolve({
+        aggregate: response.data.AggregationPayload,
+        filteredRecordCount: response.data.FilteredRecordCount,
+        payload: rows,
+        rowsPerPage,
+        searchText,
+        totalRecordCount: response.data.TotalRecordCount
+      });
+    }).catch((error) => {
+      reject(error);
+    });
+  })
+
+  public handleError(error: any) {
+    if (error.status === 404) {
+      throw new Error('Keys were not found');
+    } else if (error.status === 500) {
+      throw new Error('Internal server error');
+    }
+  }
+
+  public isValidResponse(response: object) {
+    const expectedStructure: any = {
+      AggregationPayload: null,
+      Counter: null,
+      CurrentPage: null,
+      FilteredRecordCount: null,
+      Payload: null,
+      TotalPages: null,
+      TotalRecordCount: null
+    };
+
+    const expectedStructureKeys = Object.keys(expectedStructure).sort();
+    const responseKeys = Object.keys(response).sort();
+
+    return JSON.stringify(expectedStructureKeys) === JSON.stringify(responseKeys);
+  }
+
+  public _updateDataStream(rowsPerPage: number, page: number, searchText: number) {
+    this.getAllRecords(rowsPerPage, page, searchText)
+      .then( (data) => {
+        this.dataStream.onNext(data);
+      })
+      .catch( (error) => {
+        this.handleError(error);
+      }) ;
+  }
+
+  private normalizeColumns = (columns: any[]) =>
+    columns.map((column) => {
       const obj = Object.assign({}, RemoteDataSource.defaultColumnValues, column);
-      if(column.Filtering){
+      if (column.Filtering) {
         obj.Filter = {
           Argument: [],
           HasFilter: false,
@@ -43,84 +122,10 @@ class RemoteDataSource {
           OptionsUrl: null,
           Text: null
         };
-      }      
+      }
       delete obj.Filtering;
       return obj;
-    });   
-
-  getAllRecords = (rowsPerPage: number, page: number, searchText: number) => new Promise((resolve :Function, reject:Function) => {
-    const request = {
-      'Count': this.counter++,
-      'Columns': this.columns,
-      'Skip': page * rowsPerPage,
-      'Take': rowsPerPage,
-      'Search': { 'Text': searchText ? searchText : '', 'Operator': 'Auto' },
-      'TimezoneOffset': 360
-    };
-  
-    Axios.post(this.url, request).then(response => {
-      if(response.data === undefined || !this.isValidResponse(response.data))
-        throw 'It\'s not a valid Tubular response object';
-        
-      const data = response.data.Payload;
-      const rows = data.map((row:any) => {
-        const obj: any = {};
-        
-        this.columns.forEach((column: any, key: any) => {
-          obj[column.Name] = row[key] || row[column.Name];
-        });
-        
-        return obj;
-      });
-      
-      resolve({ 
-        payload: rows,
-        filteredRecordCount: response.data.FilteredRecordCount,
-        totalRecordCount: response.data.TotalRecordCount,
-        aggregate: response.data.AggregationPayload,
-        searchText: searchText,
-        rowsPerPage: rowsPerPage
-      });
-    }).catch(error => {
-      reject(error);
-    });
-  })
-
-  handleError(error: any) {
-    if(error.status === 404) {
-      throw 'Keys were not found';
-    }
-    else if(error.status === 500) {
-      throw 'Internal server error';
-    }
-  }
-
-  isValidResponse(response: Object) {
-    const expectedStructure : any = {
-      Counter: null,
-      Payload: null,
-      TotalRecordCount: null, 
-      FilteredRecordCount: null,
-      TotalPages: null,
-      CurrentPage: null,
-      AggregationPayload: null
-    };
-
-    const expectedStructureKeys = Object.keys(expectedStructure).sort();
-    const responseKeys = Object.keys(response).sort();
-
-    return JSON.stringify(expectedStructureKeys) === JSON.stringify(responseKeys);
-  }
-    
-  _updateDataStream(rowsPerPage: number, page: number, searchText: number) {
-    this.getAllRecords(rowsPerPage, page, searchText)
-      .then( data => {
-        this.dataStream.onNext(data);
-      })
-      .catch( error => {
-        this.handleError(error);
-      }) ;
-  }
+    })
 }
 
 module.exports = RemoteDataSource;
