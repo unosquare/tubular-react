b60537490e5d9b365c6a138bbce18b4be6046eff
Juan Alcala <juan.alcala@unosquare.com>
2018-02-21 16:51:57 -0600

    Fix export

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import DataGrid from './DataGrid';
-import RemoteDataSource from './Grid/RemoteDataSource';
-export default DataGrid;
-
-export {
-  RemoteDataSource
-};
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import DataGrid,
+{
+  AggregateFunctions,
+  ColumnDataType,
+  ColumnModel,
+  ColumnSortDirection,
+  LocalDataSource,
+  RemoteDataSource
+} from './DataGrid';
+
+export default DataGrid;
+
+export {
+  AggregateFunctions,
+  ColumnDataType,
+  ColumnModel,
+  ColumnSortDirection,
+  LocalDataSource,
+  RemoteDataSource
+};
