f64142a217d199ff6481dd3b4a2f8220036e1d0c
Armando Cifuentes <32103862+ArCiGoUnoSquare@users.noreply.github.com>
2018-02-23 09:45:20 -0600

    Add unit test for None case. Add missing statements in the beforeEach of the string filtering suite tests (#114)

--- test/utils/LocalDataSourceMocks.ts
+++ test/utils/LocalDataSourceMocks.ts
@@ -1092,6 +1092,81 @@ const expectedPayloadNotEndsWithString = [
   }
 ];
 
+// Mocks for unit tests for date columns
+
+const expectedPayloadNoneDate = [
+  {
+    OrderID: 1,
+    CustomerName: 'Microsoft',
+    ShippedDate: '2016-03-19T19:00:00',
+    ShipperCity: 'Guadalajara, JAL, Mexico',
+    Amount: 300
+  },
+  {
+    OrderID: 2,
+    CustomerName: 'Microsoft',
+    ShippedDate: '2016-11-08T18:00:00',
+    ShipperCity: 'Los Angeles, CA, USA',
+    Amount: 9
+  },
+  {
+    OrderID: 3,
+    CustomerName: 'Unosquare LLC',
+    ShippedDate: '2016-11-08T18:00:00',
+    ShipperCity: 'Guadalajara, JAL, Mexico',
+    Amount: 92
+  },
+  {
+    OrderID: 4,
+    CustomerName: 'Vesta',
+    ShippedDate: '2016-03-19T19:00:00',
+    ShipperCity: 'Portland, OR, USA',
+    Amount: 300
+  },
+  {
+    OrderID: 5,
+    CustomerName: 'Super La Playa',
+    ShippedDate: '2016-04-23T10:00:00',
+    ShipperCity: 'Leon, GTO, Mexico',
+    Amount: 174
+  },
+  {
+    OrderID: 6,
+    CustomerName: 'OXXO',
+    ShippedDate: '2016-12-22T08:00:00',
+    ShipperCity: 'Guadalajara, JAL, Mexico',
+    Amount: 92
+  },
+  {
+    OrderID: 7,
+    CustomerName: 'Super La Playa',
+    ShippedDate: '2016-03-19T19:00:00',
+    ShipperCity: 'Portland, OR, USA',
+    Amount: 300
+  },
+  {
+    OrderID: 8,
+    CustomerName: 'Super La Playa',
+    ShippedDate: '2016-04-23T10:00:00',
+    ShipperCity: 'Leon, GTO, Mexico',
+    Amount: 15
+  },
+  {
+    OrderID: 9,
+    CustomerName: 'OXXO',
+    ShippedDate: '2016-12-22T08:00:00',
+    ShipperCity: 'Guadalajara, JAL, Mexico',
+    Amount: 92
+  },
+  {
+    OrderID: 10,
+    CustomerName: 'Vesta',
+    ShippedDate: '2016-03-19T19:00:00',
+    ShipperCity: 'Portland, OR, USA',
+    Amount: 300
+  }
+];
+
 export {
   expectedPayloadTextSearchVesta,
   expectedPayloadMultipleSort,
@@ -1112,5 +1187,6 @@ export {
   expectedPayloadStartsWithString,
   expectedPayloadNotStartsWithString,
   expectedPayloadEndsWithString,
-  expectedPayloadNotEndsWithString
+  expectedPayloadNotEndsWithString,
+  expectedPayloadNoneDate
 };
