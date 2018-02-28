a6680ddfedf0c83d23ba9accd7cee5bc1e048e3f
Armando Cifuentes <armando.cifuentes@unosquare.com>
2018-02-28 09:46:22 -0600

    Add unit test for connect()

--- test/LocalDataSource.spec.tsx
+++ test/LocalDataSource.spec.tsx
@@ -19,6 +19,7 @@ import {
 import localData from './utils/localData';
 import {
   expectedLocaDataSourcelResponse,
+  expectedLocalDataSourceResponseConnect,
   expectedPayloadBetweenDate,
   expectedPayloadBetweenNumeric,
   expectedPayloadContainsString,
@@ -527,6 +528,21 @@ describe('LocalDataSource', () => {
 
       dataSource.getAllRecords(10, 0, '').then((response: GridResponse) => {
         expect(response.Aggregate.CustomerName).to.be.equal(22);
+        done();
+      });
+    });
+  });
+
+  describe('When connect is called', () => {
+    const dataSource = new LocalDataSource(localData, simpleColumnsSample);
+
+    it('should return a payload', (done) => {
+      dataSource.connect(10, 0, '').skip(1).subscribe((response: GridResponse) => {
+        expect(response.Payload).to.deep.equal(expectedLocalDataSourceResponseConnect.Payload);
+        expect(response.FilteredRecordCount).to.deep.equal(expectedLocalDataSourceResponseConnect.FilteredRecordCount);
+        expect(response.TotalRecordCount).to.deep.equal(expectedLocalDataSourceResponseConnect.TotalRecordCount);
+        done();
+      }, (error: any) => {
         done();
       });
     });
--- test/utils/LocalDataSourceMocks.ts
+++ test/utils/LocalDataSourceMocks.ts
@@ -75,6 +75,85 @@ const expectedLocaDataSourcelResponse = {
   TotalRecordCount: 22
 };
 
+const expectedLocalDataSourceResponseConnect = {
+  Aggregate: { CustomerName: 22 },
+  FilteredRecordCount: 22,
+  Payload: [
+    {
+      OrderID: 1,
+      CustomerName: 'Microsoft',
+      ShippedDate: '2016-03-19T19:00:00',
+      ShipperCity: 'Guadalajara, JAL, Mexico',
+      Amount: 300
+    },
+    {
+      OrderID: 2,
+      CustomerName: 'Microsoft',
+      ShippedDate: '2016-11-08T18:00:00',
+      ShipperCity: 'Los Angeles, CA, USA',
+      Amount: 9
+    },
+    {
+      OrderID: 3,
+      CustomerName: 'Unosquare LLC',
+      ShippedDate: '2016-11-08T18:00:00',
+      ShipperCity: 'Guadalajara, JAL, Mexico',
+      Amount: 92
+    },
+    {
+      OrderID: 4,
+      CustomerName: 'Vesta',
+      ShippedDate: '2016-03-19T19:00:00',
+      ShipperCity: 'Portland, OR, USA',
+      Amount: 300
+    },
+    {
+      OrderID: 5,
+      CustomerName: 'Super La Playa',
+      ShippedDate: '2016-04-23T10:00:00',
+      ShipperCity: 'Leon, GTO, Mexico',
+      Amount: 174
+    },
+    { OrderID: 6,
+      CustomerName: 'OXXO',
+      ShippedDate: '2016-12-22T08:00:00',
+      ShipperCity: 'Guadalajara, JAL, Mexico',
+      Amount: 92
+    },
+    {
+      OrderID: 7,
+      CustomerName: 'Super La Playa',
+      ShippedDate: '2016-03-19T19:00:00',
+      ShipperCity: 'Portland, OR, USA',
+      Amount: 300
+    },
+    {
+      OrderID: 8,
+      CustomerName: 'Super La Playa',
+      ShippedDate: '2016-04-23T10:00:00',
+      ShipperCity: 'Leon, GTO, Mexico',
+      Amount: 15
+    },
+    {
+      OrderID: 9,
+      CustomerName: 'OXXO',
+      ShippedDate: '2016-12-22T08:00:00',
+      ShipperCity: 'Guadalajara, JAL, Mexico',
+      Amount: 92
+    },
+    {
+      OrderID: 10,
+      CustomerName: 'Vesta',
+      ShippedDate: '2016-03-19T19:00:00',
+      ShipperCity: 'Portland, OR, USA',
+      Amount: 300
+    }
+  ],
+  RowsPerPage: 10,
+  SearchText: '',
+  TotalRecordCount: 22
+};
+
 // Mock for unit test when page is set to 1 (Page 2)
 
 const expectedPayloadPage2 = [
@@ -1591,6 +1670,7 @@ const expectedPayloadLtDate = [
 ];
 
 export {
+  expectedLocalDataSourceResponseConnect,
   expectedPayloadTextSearchVesta,
   expectedPayloadMultipleSort,
   expectedPayloadDescSortByOrderID,
