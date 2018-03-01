8414ea21002fdecdd2ac84838a348312346334bd
Leonardi Pérez <marcolpr98@gmail.com>
2018-03-01 08:19:11 +0100

    Fixed syntax and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 const withTypescript = require('@zeit/next-typescript');
 
 module.exports = withTypescript({
+  assetPrefix: isProd ? 'https://unosquare.github.io/tubular-react/' : '',
   webpack: (config, { dev }) => {
     config.module.rules.push({ test: /\.ts|\.tsx$/, loader: ['ts-loader'], include: __dirname });
     return config;
@@ -13,8 +14,7 @@ module.exports = withTypescript({
       '/src/docs/modules/pages/Documentation/ColumnModel': { page: '/tubular-react/Documentation/ColumnModel' },
       '/src/docs/modules/pages/Documentation/DataSource': { page: '/tubular-react/Documentation/DataSource' },
       '/src/docs/modules/pages/Documentation/Getting-Started': { page: '/tubular-react/Documentation/Getting-Started' },
-      '/src/docs/modules/pages/Documentation/Props': { page: '/tubular-react/Documentation/Props' },
+      '/src/docs/modules/pages/Documentation/Props': { page: '/tubular-react/Documentation/Props' }
     }
-    assetPrefix: isProd ? 'https://unosquare.github.io/tubular-react/' : ''
   },
 });
