5201c8b90a7e1c2129c9d0db66193a1b3441e8e3
Armando Cifuentes <armando.cifuentes@unosquare.com>
2018-03-02 10:29:46 -0600

    Solve issues of tslint

--- test/TablePaginationActions.spec.tsx
+++ test/TablePaginationActions.spec.tsx
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import * as Enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 import IconButton from 'material-ui/IconButton';
@@ -9,7 +9,7 @@ import TablePaginationActions from '../src/DataGrid/TablePaginationActions';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('TablePaginationActions', () => {
-  const noop = () => {};
+  const noop = () => { return ; };
 
   let shallow;
   let mountedTablePaginationActions;
@@ -27,10 +27,10 @@ describe('TablePaginationActions', () => {
     return mountedTablePaginationActions;
   };
 
-  it('should render 9 \'<IconButton />\' when has 10 of 500 records to show by page', ()=> {
+  it('should render 9 \'<IconButton />\' when has 10 of 500 records to show by page', () => {
     const wrapper = shallow(
       <TablePaginationActions
-        classes={}
+        classes={{}}
         count={500}
         page={0}
         rowsPerPage={10}
@@ -44,7 +44,7 @@ describe('TablePaginationActions', () => {
   it('should have the disabled prop from \'First Page\' as true', () => {
     const wrapper = shallow(
       <TablePaginationActions
-        classes={}
+        classes={{}}
         count={500}
         page={0}
         rowsPerPage={10}
@@ -55,13 +55,13 @@ describe('TablePaginationActions', () => {
     const firstPage = wrapper.find(IconButton).at(0);
 
     firstPage.simulate('click');
-    expect(firstPage.props().disabled).to.be.true;
+    assert.isTrue(firstPage.props().disabled);
   });
 
   it('should have the disabled prop from \'First Page\' as false', () => {
     const wrapper = shallow(
       <TablePaginationActions
-        classes={}
+        classes={{}}
         count={500}
         page={2}
         rowsPerPage={10}
@@ -72,13 +72,13 @@ describe('TablePaginationActions', () => {
     const firstPage = wrapper.find(IconButton).at(0);
 
     firstPage.simulate('click');
-    expect(firstPage.props().disabled).to.be.false;
+    assert.isFalse(firstPage.props().disabled);
   });
 
   it('should have the disabled prop from \'Last Page\' as true', () => {
     const wrapper = shallow(
       <TablePaginationActions
-        classes={}
+        classes={{}}
         count={500}
         page={49}
         rowsPerPage={10}
@@ -89,13 +89,13 @@ describe('TablePaginationActions', () => {
     const lastPage = wrapper.find(IconButton).at(8);
 
     lastPage.simulate('click');
-    expect(lastPage.props().disabled).to.be.true;
+    assert.isTrue(lastPage.props().disabled);
   });
 
   it('should have the disabled prop from \'Last Page\' as false', () => {
     const wrapper = shallow(
       <TablePaginationActions
-        classes={}
+        classes={{}}
         count={500}
         page={47}
         rowsPerPage={10}
@@ -106,13 +106,13 @@ describe('TablePaginationActions', () => {
     const lastPage = wrapper.find(IconButton).at(8);
 
     lastPage.simulate('click');
-    expect(lastPage.props().disabled).to.be.false;
+    assert.isFalse(lastPage.props().disabled);
   });
 
   it('should have the disabled prop from \'Previous Page\' as false', () => {
     const wrapper = shallow(
       <TablePaginationActions
-        classes={}
+        classes={{}}
         count={500}
         page={49}
         rowsPerPage={10}
@@ -123,13 +123,13 @@ describe('TablePaginationActions', () => {
     const previousPage = wrapper.find(IconButton).at(1);
 
     previousPage.simulate('click');
-    expect(previousPage.props().disabled).to.be.false;
+    assert.isFalse(previousPage.props().disabled);
   });
 
   it('should have the disabled prop from \'Previous Page\' as true', () => {
     const wrapper = shallow(
       <TablePaginationActions
-        classes={}
+        classes={{}}
         count={500}
         page={0}
         rowsPerPage={10}
@@ -140,7 +140,7 @@ describe('TablePaginationActions', () => {
     const previousPage = wrapper.find(IconButton).at(1);
 
     previousPage.simulate('click');
-    expect(previousPage.props().disabled).to.be.true;
+    assert.isTrue(previousPage.props().disabled);
   });
 
   beforeEach( () => {
