abe7afdbff585d4da5f890ab8ad6ff6148fa9f31
Armando Cifuentes <armando.cifuentes@unosquare.com>
2018-03-06 13:37:36 -0600

    Add test cases for filter dialog

--- test/GridHeader.spec.tsx
+++ test/GridHeader.spec.tsx
@@ -4,6 +4,7 @@ import { assert, expect } from 'chai';
 import * as Enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import IconButton from 'material-ui/IconButton';
 import Table, { TableBody, TableCell, TableFooter, TableHead, TableRow } from 'material-ui/Table';
 import { createMount, createShallow } from 'material-ui/test-utils';
 import * as React from 'react';
@@ -41,17 +42,54 @@ describe('<GridHeader />', () => {
 
   it('should render a row', () => {
     const wrapper = shallow(gridHeader).find(TableRow);
+
     expect(wrapper).to.have.lengthOf(1);
   });
 
   it('should render n columns', () => {
     const wrapper = shallow(gridHeader).find(TableRow).find(TableCell);
+
     expect(wrapper).to.have.lengthOf(5);
   });
 
   it('should render a dialog', () => {
     const wrapper = shallow(gridHeader).find(Dialog);
+
     expect(wrapper).to.have.lengthOf(1);
+  });
+
+  describe('When filter dialog has been clicked', () => {
+    it('should update the state of \'open\' to false when is closed', () => {
+      const wrapper = shallow(gridHeader);
+      wrapper.setState({ open: false });
+      wrapper.update();
+
+      assert.isFalse(wrapper.state().open);
+    });
+
+    it('should update the state of \'open\' to true when is open', () => {
+      const wrapper = shallow(gridHeader);
+      const firstCell = wrapper.find(TableRow).find(TableCell);
+      const firstIconButton = wrapper.find(TableRow).find(TableCell).find(IconButton).at(0);
+
+      firstIconButton.simulate('blur');
+      wrapper.setState({
+        activeFilter: 'Equals',
+        activeFilterColumn: 'OrderID',
+        columnType: 'numeric',
+        firstFilterValue: '6',
+        open: true,
+        secondFilterValue: ''
+      });
+      wrapper.update();
+
+      assert.isTrue(wrapper.find(Dialog).props().open);
+      expect(wrapper.state().activeFilter).to.be.equal('Equals');
+      expect(wrapper.state().activeFilterColumn).to.be.equal('OrderID');
+      expect(wrapper.state().columnType).to.be.equal('numeric');
+      expect(wrapper.state().firstFilterValue).to.be.equal('6');
+      assert.isEmpty(wrapper.state().secondFilterValue);
+    });
   });
 
   beforeEach( () => {
