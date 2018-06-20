b32314f18af273fffc7c0ad2211bb09d7b4ab6f1
AnyxLeo <anyx.leo@gmail.com>
2018-06-20 16:07:41 -0500

    test remote data source

--- test/RemoteDataSource.spec.tsx
+++ test/RemoteDataSource.spec.tsx
@@ -1,113 +1,28 @@
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import { createShallow } from '@material-ui/core/test-utils';
+import * as React from 'react';
 import RemoteDataSource from '../src/DataGrid/DataSource/RemoteDataSource';
+import ToolbarOptions from '../src/DataGrid/Models/ToolbarOptions';
 import { validColumnsSample } from './utils/columns';
-import {
-  invalidResponseStructure,
-  onlyMicrosoftExpected,
-  simpleRecordsExpected,
-  twentyRecordsExpected,
-  validResponseStructure
-} from './utils/data';
 
 describe('RemoteDataSource', () => {
-  const dataSource = new RemoteDataSource('url', validColumnsSample);
+  let shallow;
+  let remoteDataSource;
+  let  toolbarOptions;
 
-  describe('isValidResponse()', () => {
-    test('should return true when expectedStructure is valid', () => {
-      expect(dataSource.isValidResponse(validResponseStructure)).toBe(true);
-    });
-
-    test('should return false when expectedStructure is invalid', () => {
-      expect(dataSource.isValidResponse(invalidResponseStructure)).toBe(false);
-    });
+  beforeAll(() => {
+    toolbarOptions = new ToolbarOptions();
+    remoteDataSource = (
+      <RemoteDataSource
+        source='url'
+        columns={validColumnsSample}
+        itemsPerPage={toolbarOptions.itemsPerPage}
+      />
+    );
+    shallow = createShallow({ dive: true });
   });
-
-  describe('When columns structure is valid', () => {
-    let mock;
-
-    beforeEach(() => {
-        mock = new MockAdapter(axios);
-    });
-
-    afterEach(() => {
-        mock.reset();
-    });
-
-    describe('When 20 records are requested', () => {
-      beforeEach( () => {
-        mock.reset();
-        mock.onPost('url').reply(200, {
-          ...twentyRecordsExpected
-        });
-      });
-
-      test('Should return a payload with 20 records', () => {
-         return dataSource.getAllRecords(20, 0, '').then((e: any) => {
-            expect(e.Payload).toHaveLength(20);
-         });
-      });
-    });
-
-    describe('When search input is Microsoft', () => {
-      beforeEach( () => {
-        mock.reset();
-        mock.onPost('url').reply(200, {
-          ...onlyMicrosoftExpected
-        });
-      });
-
-      test('Should return a payload with only Microsoft records', () => dataSource.getAllRecords(10, 0, 'Microsoft')
-        .then((r: any) => {
-          expect(r.Payload).toEqual(onlyMicrosoftExpected.Payload);
-          expect(r.FilteredRecordCount).toEqual(onlyMicrosoftExpected.FilteredRecordCount);
-          expect(r.TotalRecordCount).toEqual(onlyMicrosoftExpected.TotalRecordCount);
-        }));
-    });
-
-    describe('When retrieveData() is called', () => {
-      describe('When the response is invalid', () => {
-        beforeEach( () => {
-          mock.reset();
-          mock.onPost('url').reply(200, {
-            ...simpleRecordsExpected
-          });
-        });
-
-        test('Should return a payload', (done) => {
-          dataSource.retrieveData(10, 0, '')
-            .skip(1).subscribe((r) => {
-              expect(r.Payload).toEqual(simpleRecordsExpected.Payload);
-              expect(r.FilteredRecordCount).toEqual(simpleRecordsExpected.FilteredRecordCount);
-              expect(r.TotalRecordCount).toEqual(simpleRecordsExpected.TotalRecordCount);
-              done();
-            }, (error: any) => {
-              done();
-            });
-        });
-      });
-
-      describe('When the response is invalid', () => {
-        const dtSource = new RemoteDataSource('url', validColumnsSample);
-
-        beforeEach( () => {
-          mock.reset();
-          mock.onPost('url').reply(200, {
-            AggregationPayload: simpleRecordsExpected.AggregationPayload,
-            Counter: simpleRecordsExpected.Counter,
-            CurrentPage: simpleRecordsExpected.CurrentPage,
-            FilteredRecordCount: simpleRecordsExpected.FilteredRecordCount
-          });
-        });
-
-        test('Should throw an error', (done) => {
-          dtSource.retrieveData(10, 0, '')
-            .subscribe((r) => r, (error: any) => {
-              expect(error.message).toBe('It\'s not a valid Tubular response object');
-              done();
-            });
-        });
-      });
-    });
+  test('Renders RemoteDataSource', () => {
+      const component = shallow(remoteDataSource).inst;
+      console.log('Entra a la prueba');
+      console.log(component);
   });
 });
