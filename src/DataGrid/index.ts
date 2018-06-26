3ecace2a6fc613547af66f704287e8a91e0abc83
Geo Perez <geovanni.perez@gmail.com>
2018-06-26 15:57:16 -0500

    Fix imports

--- src/DataGrid/index.ts
+++ src/DataGrid/index.ts
@@ -1,6 +1,6 @@
 import DataGrid from './DataGrid';
-import LocalDataSource from './DataSource/LocalDataSource';
-import RemoteDataSource from './DataSource/RemoteDataSource';
+import withLocalDataSource from './DataSource/LocalDataSource';
+import withRemoteDataSource from './DataSource/RemoteDataSource';
 import { AggregateFunctions, ColumnDataType, ColumnSortDirection } from './Models/Column';
 import ColumnModel from './Models/ColumnModel';
 import ToolbarOptions from './Models/ToolbarOptions';
@@ -8,5 +8,5 @@ import ToolbarOptions from './Models/ToolbarOptions';
 export default DataGrid;
 export {
   AggregateFunctions, ColumnDataType, ColumnModel, ColumnSortDirection,
-  LocalDataSource, RemoteDataSource, ToolbarOptions
+  withLocalDataSource, withRemoteDataSource, ToolbarOptions
 };
