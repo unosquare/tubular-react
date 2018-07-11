eed37d11783b9ef9b989805d4d1a013a73f98bc8
AnyxLeo <anyx.leo@gmail.com>
2018-07-10 19:10:14 -0500

    Home view

--- srcdocs/pages/Documentation/Home.tsx
+++ srcdocs/pages/Documentation/Home.tsx
@@ -44,75 +44,84 @@ export default withStyles(styles)((props) => {
     return (
         <div>
             <Grid container={true} spacing={24} className={classes.container}>
-                <Grid item={true} xs={12} md={6}>
-                    <Paper className={classes.paper}>
-                        <Typography variant='display1'>
-                            What is it?
+                <Paper className={classes.paper}>
+                    <Typography variant='display1'>
+                        What is it?
                         </Typography>
-                        <Divider />
-                        <Typography paragraph={true} variant='subheading' className={classes.content}>
-                            Tubular React is a set of ReactJS components designed to rapidly build modern
-                            web applications.
-                            The centerpiece of Tubular is its fully templated grid with lots of features such as
-                            server-side pagination, searching text, multi-column sorting, and filtering,
-                            built-in export to CSV or been printed.
-                            Another very styled and useful component is the grid list that renders a set of cards with
-                            the general information allowing good data visualization and quickly searching,
-                            besides it has almost the same grid functionalities.
+                    <Divider />
+                    <Typography paragraph={true} variant='subheading' className={classes.content}>
+                        Tubular React is a set of ReactJS components designed to rapidly build modern
+                        web applications.
+                        The centerpiece of Tubular is its fully templated grid with lots of features such as
+                        server-side pagination, searching text, multi-column sorting, and filtering,
+                        built-in export to CSV or been printed.
+                        Another very styled and useful component is the grid list that renders a set of cards with
+                        the general information allowing good data visualization and quickly searching,
+                        besides it has almost the same grid functionalities.
                         </Typography>
-                    </Paper>
-                </Grid>
-                <Grid item={true} xs={12} md={6}>
-                    <Paper className={classes.paper}>
-                        <Typography variant='display1'>
-                            Features
+                    <br />
+                    <Typography variant='display1'>
+                        Features
                         </Typography>
-                        <Divider />
-                        <Typography paragraph={true} variant='subheading' className={classes.content}>
-                            The main component is a grid with multiple options:
-                            <ul>
-                                <li>Define a custom layout for columns and cells using render methods.</li>
-                                <li>
-                                    Use a remote or local datasource.
-                                    Remote datasource use a specific Request and Response format.
+                    <Divider />
+                    <Typography paragraph={true} variant='subheading' className={classes.content}>
+                        <Typography variant='headline'>
+                            The main component is a <em>grid</em> with multiple options:
+                        </Typography>
+                        <ul>
+                            <li>Define a custom layout for columns and cells using render methods.</li>
+                            <li>
+                                Use a remote or local datasource.
+                                Remote datasource use a specific Request and Response format.
                                 </li>
-                                <li>Sort and filter multiple columns.</li>
-                                <li>Free-text search of string columns.</li>
-                                <li>Page data. Remote data is paged in the server side.</li>
-                                <li>Export data to a CSV file.</li>
-                                <li>Print data.</li>
-                            </ul>
+                            <li>Sort and filter multiple columns.</li>
+                            <li>Free-text search of string columns.</li>
+                            <li>Page data. Remote data is paged in the server side.</li>
+                            <li>Export data to a CSV file.</li>
+                            <li>Print data.</li>
+                        </ul>
+                        <br />
+                        The <em>grid</em> component offers all you can look for an easy grid table and more.
+                        <br /><br />
+                        <Typography variant='headline'>
+                            <em>Grid list</em> component allows you to perform some of the same options as:
                         </Typography>
-                    </Paper>
-                </Grid>
-                <Grid item={true} xs={12} md={6}>
-                    <Paper className={classes.paperSmall}>
-                        <Typography variant='display1'>
-                            Dependencies
+                        <ul>
+                            <li>Define a custom layout for columns and cells.</li>
+                            <li>
+                                Use a remote or local datasource.
+                            </li>
+                            <li>Free-text search of string columns.</li>
+                            <li>Page data. Remote data is paged in the server side.</li>
+                        </ul>
+                        <br />
+                        The <em>grid list</em> component represents another option to show your data.<br />
+                        <em>Grid list</em> provide a styled and fancy view over your data, which helps
+                            you to quickly find information.
+                    </Typography>
+                    <br />
+                    <Typography variant='display1'>
+                        Dependencies
                         </Typography>
-                        <Divider />
-                        <Typography paragraph={true} variant='subheading' >
-                            <ul>
-                                <li>
-                                    <a href='https://material-ui-next.com'>Material-UI Next - Version: Beta 34.</a>
-                                </li>
-                            </ul>
+                    <Divider />
+                    <Typography paragraph={true} variant='subheading' >
+                        <ul>
+                            <li>
+                                <a href='https://material-ui-next.com'>Material-UI Next - Version: Beta 34.</a>
+                            </li>
+                        </ul>
+                    </Typography>
+                    <br />
+                    <Typography variant='display1'>
+                        npm Installation
                         </Typography>
-                    </Paper>
-                </Grid>
-                <Grid item={true} xs={12} md={6}>
-                    <Paper className={classes.paperSmall}>
-                        <Typography variant='display1'>
-                            npm Installation
-                        </Typography>
-                        <Divider />
-                        <Typography paragraph={true} variant='subheading' className={classes.content}>
-                            <SyntaxHighligther language='tsx' style={docco} className={classes.code}>
-                                $ npm install tubular-react --save
+                    <Divider />
+                    <Typography paragraph={true} variant='subheading' className={classes.content}>
+                        <SyntaxHighligther language='tsx' style={docco} className={classes.code}>
+                            $ npm install tubular-react --save
                             </SyntaxHighligther>
-                        </Typography>
-                    </Paper>
-                </Grid>
+                    </Typography>
+                </Paper>
             </Grid>
         </div>
     );
