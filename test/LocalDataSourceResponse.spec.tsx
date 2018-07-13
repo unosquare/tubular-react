148c9eaf70a087e2fb8727df7c147c80eb8390a4
Geo Perez <geovanni.perez@gmail.com>
2018-07-13 15:40:27 -0500

    WIP

--- test/LocalDataSourceResponse.spec.tsx
+++ test/LocalDataSourceResponse.spec.tsx
@@ -1,10 +1,18 @@
-import * as React from 'react';
-
 import LocalDataSourceResponse from '../src/DataSource/LocalDataSourceResponse';
 import GridRequest from '../src/Models/GridRequest';
 import GridResponse from '../src/Models/GridResponse';
 import { simpleColumnsSample } from './utils/columns';
 import { localData } from './utils/localData';
+import { simpleRequest } from './utils/requests';
+import { simpleResponse } from './utils/responses';
+
+const cases = [
+  ['simple', simpleRequest, simpleResponse],
+  ['with PageSize 20', {}, {}],
+  ['with PageSize -1', {}, {}],
+  ['with SearchText Microsoft', {}, {}],
+  // ... continue pls
+];
 
 describe('LocalDataSourceResponse', () => {
   test('Should return a grid response', () => {
@@ -12,5 +20,9 @@ describe('LocalDataSourceResponse', () => {
         LocalDataSourceResponse.getResponse(
             new GridRequest(simpleColumnsSample, -1, 0, ''),
             localData)).toBeInstanceOf(GridResponse);
+  });
+
+  test.each(cases)('Should return response %s', (id : any, request : GridRequest, response : GridResponse) => {
+    expect(LocalDataSourceResponse.getResponse(request, localData)).toEqual(response);
   });
 });
--- test/utils/responses.ts
+++ test/utils/responses.ts
@@ -1,15 +1,3 @@
-const data = [
-  [1, 'Microsoft', '2016-03-19T19:00:00', 'Guadalajara, JAL, Mexico', 300],
-  [2, 'Microsoft', '2016-04-23T10:00:00', 'Guadalajara, JAL, Mexico', 0],
-  [3, 'Microsoft', '2016-12-22T08:00:00', 'Guadalajara, JAL, Mexico', 300],
-  [4, 'Unosquare LLC', '2016-02-01T18:00:00', 'Los Angeles, CA, USA', 0],
-  [5, 'Microsoft', '2016-11-10T18:00:00', 'Guadalajara, JAL, Mexico', 92],
-  [6, 'Unosquare LLC', '2016-11-06T18:00:00', 'Los Angeles, CA, USA', 18],
-  [7, 'Unosquare LLC', '2016-11-11T18:00:00', 'Leon, GTO, Mexico', 50],
-  [8, 'Unosquare LLC', '2016-11-08T18:00:00', 'Portland, OR, USA', 9],
-  [9, 'Vesta', '2016-11-07T18:00:00', 'Guadalajara, JAL, Mexico', 108],
-  [10, 'Unosquare LLC', '2016-11-05T18:00:00', 'Portland, OR, USA', 15]
-];
 
 const twentyRecordsExpected = {
   AggregationPayload: { CustomerName: 500 },
@@ -426,7 +414,7 @@ const descendingExpected = {
   TotalRecordCount: 500
 };
 
-const expected = {
+const simpleResponse = {
   aggregate: { CustomerName: 500 },
   filteredRecordCount: 500,
   payload: [
@@ -526,9 +514,8 @@ const invalidResponseStructure = {
 };
 
 export {
-  data,
   descendingExpected,
-  expected,
+  simpleResponse,
   invalidResponseStructure,
   onlyMicrosoftExpected,
   page2Expected,
