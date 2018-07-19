17c8f2224978e2f5f9e00e7718dc67b10924ca4c
AnyxLeo <anyx.leo@gmail.com>
2018-07-19 16:04:51 -0500

    Request partial object

--- src/DataSource/RemoteDataSource.ts
+++ src/DataSource/RemoteDataSource.ts
@@ -4,7 +4,7 @@ import BaseDataSource from './BaseDataSource';
 import IBaseDataSourceState from './IBaseDataSourceState';
 
 const expectedStructureKeys = JSON.stringify([
-  'Aggregate',
+  'AggregationPayload',
   'Counter',
   'CurrentPage',
   'FilteredRecordCount',
@@ -12,7 +12,7 @@ const expectedStructureKeys = JSON.stringify([
   'TotalPages',
   'TotalRecordCount']);
 
-const withRemoteDataSource = (WrappedComponent: any, columns: any, url: string, itemsPerPage = 10) => {
+const withRemoteDataSource = (WrappedComponent: any, columns: any, request: string | Request, itemsPerPage = 10) => {
   return class extends BaseDataSource {
     public setInitialState(value: any): IBaseDataSourceState {
       return {
@@ -26,25 +26,28 @@ const withRemoteDataSource = (WrappedComponent: any, columns: any, url: string, 
       return WrappedComponent;
     }
 
-    public getAllRecords(request: GridRequest): Promise<object> {
-     return fetch(url, {
-        body: JSON.stringify(request),
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        },
-        method: 'POST'
-      })
+    public getAllRecords(gridRequest: GridRequest): Promise<object> {
+      return fetch(this.getRequest(request, gridRequest))
         .then((response) => response.json())
         .then((data) => {
           if (!this.isValidResponse(data)) {
             throw new Error('Server response is a invalid Tubular object');
           }
 
-          data.Payload = data.Payload.map((row: any) => this.parsePayload(row, request.Columns));
+          data.Payload = data.Payload.map((row: any) => this.parsePayload(row, gridRequest.Columns));
           return data;
         });
     }
 
+    public getRequest(objRequest: string | Request, gridRequest: GridRequest) {
+      if (typeof objRequest === 'string') {
+        return new Request(objRequest,
+          {body: JSON.stringify(gridRequest),
+          headers: new Headers({'Content-Type': 'application/json;charset=utf-8'}),
+          method: 'POST'});
+      }
+      return objRequest;
+    }
     public isValidResponse(data: any) {
       return data && expectedStructureKeys === JSON.stringify(Object.keys(data).sort());
     }
