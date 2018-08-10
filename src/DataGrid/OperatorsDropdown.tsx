9709afa0774f83769896b90bc4b73e33a77d8092
Geovanni Perez <geovanni.perez@gmail.com>
2018-08-10 15:56:58 -0500

    Implementing new TbCommon (#214)

--- src/DataGrid/OperatorsDropdown.tsx
+++ src/DataGrid/OperatorsDropdown.tsx
@@ -1,56 +1,12 @@
 import * as React from 'react';
-import { ColumnDataType, CompareOperators } from 'tubular-common';
 
 import { Input, MenuItem, Select } from '@material-ui/core';
+import { ColumnModel } from 'tubular-common';
 import { DataSourceContext } from '../DataSource';
 
 const dropdown = {
   marginLeft: '10%',
   width: '80%'
-};
-
-const NumericOperators = [
-  { Value: CompareOperators.NONE, Title: 'None' },
-  { Value: CompareOperators.EQUALS, Title: 'Equals' },
-  { Value: CompareOperators.BETWEEN, Title: 'Between' },
-  { Value: CompareOperators.GTE, Title: '>=' },
-  { Value: CompareOperators.GT, Title: '>' },
-  { Value: CompareOperators.LTE, Title: '<=' },
-  { Value: CompareOperators.LT, Title: '<' }
-];
-
-const StringOperators = [
-  { Value: CompareOperators.NONE, Title: 'None' },
-  { Value: CompareOperators.EQUALS, Title: 'Equals' },
-  { Value: CompareOperators.NOT_EQUALS, Title: 'Not Equals' },
-  { Value: CompareOperators.CONTAINS, Title: 'Contains' },
-  { Value: CompareOperators.NOT_CONTAINS, Title: 'Not Contains' },
-  { Value: CompareOperators.STARTS_WITH, Title: 'Starts With' },
-  { Value: CompareOperators.NOT_STARTS_WITH, Title: 'Not Starts With' },
-  { Value: CompareOperators.ENDS_WITH, Title: 'Ends With' },
-  { Value: CompareOperators.NOT_ENDS_WITH, Title: 'Not Ends With' }
-];
-
-const BooleanOperators = [
-  { Value: CompareOperators.NONE, Title: 'None' },
-  { Value: CompareOperators.EQUALS, Title: 'Equals' },
-  { Value: CompareOperators.NOT_EQUALS, Title: 'Not Equals' }
-];
-
-const getOperators = (DataType: any) => {
-  switch (DataType) {
-    case ColumnDataType.STRING:
-      return StringOperators;
-    case ColumnDataType.NUMERIC:
-    case ColumnDataType.DATE:
-    case ColumnDataType.DATE_TIME:
-    case ColumnDataType.DATE_TIME_UTC:
-      return NumericOperators;
-    case ColumnDataType.BOOLEAN:
-      return BooleanOperators;
-    default:
-      return [];
-  }
 };
 
 const OperatorsDropdown: React.SFC = () => {
@@ -64,9 +20,9 @@ const OperatorsDropdown: React.SFC = () => {
           input={<Input name={state.activeColumn.Name} />}
         >
           {
-            getOperators(state.activeColumn.DataType).map((row: any, i: number) => (
-              <MenuItem key={i} value={row.Value}>{row.Title}</MenuItem>
-            ))
+            ColumnModel.getOperators(state.activeColumn).map((row: any) =>
+              <MenuItem key={row.Value} value={row.Value}>{row.Title}</MenuItem>
+            )
           }
         </Select>}
     </DataSourceContext.Consumer>
