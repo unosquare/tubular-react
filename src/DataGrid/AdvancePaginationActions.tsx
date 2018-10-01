2052a1c15e5a9dc41a773a0e9daaf9dab2d2134e
Alexis Zapata Santana <alexis.zapata.santana@gmail.com>
2018-10-01 12:07:41 -0500

    add the last page into the pagination (#232)
    * add the last page into the pagination
    
    * Bump version

--- src/DataGrid/AdvancePaginationActions.tsx
+++ src/DataGrid/AdvancePaginationActions.tsx
@@ -1,18 +1,27 @@
 import { IconButton } from '@material-ui/core';
-import { createStyles, Theme, withStyles, WithStyles, } from '@material-ui/core/styles';
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
+} from '@material-ui/core/styles';
 
-import { FirstPage, KeyboardArrowLeft, KeyboardArrowRight, LastPage } from '@material-ui/icons';
+import {
+  FirstPage,
+  KeyboardArrowLeft,
+  KeyboardArrowRight,
+  LastPage
+} from '@material-ui/icons';
 
 import * as React from 'react';
 
-const styles = ({ spacing }: Theme) => createStyles(
-  {
+const styles = ({ spacing }: Theme) =>
+  createStyles({
     root: {
       flexShrink: 0,
       marginLeft: spacing.unit * 2.5
     }
-  }
-);
+  });
 
 interface IProps extends WithStyles<typeof styles> {
   count: number;
@@ -45,14 +54,20 @@ const getPages = (currentPage: any, totalRows: any, itemsPerPage: any) => {
   }
 
   // Add page number links
-  for (let num = startPage; num < endPage; num++) {
+  for (let num = startPage; num <= endPage; num++) {
     pages.push(num - 1);
   }
 
   return pages;
 };
 
-const AdvancePaginationActions: React.SFC<IProps> = ({ classes, count, page, rowsPerPage, onChangePage }) => {
+const AdvancePaginationActions: React.SFC<IProps> = ({
+  classes,
+  count,
+  page,
+  rowsPerPage,
+  onChangePage
+}) => {
   const pages = getPages(page, count, rowsPerPage);
   return (
     <div className={classes.root}>
@@ -72,39 +87,39 @@ const AdvancePaginationActions: React.SFC<IProps> = ({ classes, count, page, row
         <KeyboardArrowLeft />
       </IconButton>
 
-      {
-        pages.map((value) => (
-          <IconButton
-            key={value}
-            onClick={(e) => onChangePage(e, value)}
-            disabled={value >= (Math.ceil(count / rowsPerPage))}
-            aria-label={`Page ${value + 1}`}
-            color={value === page ?
-              'primary' :
-              'default'}
-          >
-            {value + 1}
-          </IconButton>))
-      }
+      {pages.map((value) => (
+        <IconButton
+          key={value}
+          onClick={(e) => onChangePage(e, value)}
+          disabled={value >= Math.ceil(count / rowsPerPage)}
+          aria-label={`Page ${value + 1}`}
+          color={value === page ? 'primary' : 'default'}
+        >
+          {value + 1}
+        </IconButton>
+      ))}
 
       <IconButton
         onClick={(e) => onChangePage(e, page + 1)}
-        disabled={page === (Math.ceil(count / rowsPerPage) - 1)}
+        disabled={page === Math.ceil(count / rowsPerPage) - 1}
         aria-label='Next Page'
       >
         <KeyboardArrowRight />
       </IconButton>
 
       <IconButton
-        onClick={(e) => onChangePage(e, Math.max(0, Math.ceil(count / rowsPerPage) - 1))}
-        disabled={page === (Math.ceil(count / rowsPerPage) - 1)}
+        onClick={(e) =>
+          onChangePage(e, Math.max(0, Math.ceil(count / rowsPerPage) - 1))
+        }
+        disabled={page === Math.ceil(count / rowsPerPage) - 1}
         aria-label='Last Page'
       >
         <LastPage />
       </IconButton>
-
     </div>
   );
 };
 
-export default withStyles(styles, { withTheme: true })(AdvancePaginationActions);
+export default withStyles(styles, { withTheme: true })(
+  AdvancePaginationActions
+);
