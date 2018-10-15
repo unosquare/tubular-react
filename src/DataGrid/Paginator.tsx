8c00fdc621d0f34d9d681aac1b4890b464879303
Alexis Zapata Santana <alexis.zapata.santana@gmail.com>
2018-10-15 17:43:11 -0500

    return to the previous state

--- src/DataGrid/Paginator.tsx
+++ src/DataGrid/Paginator.tsx
@@ -9,22 +9,32 @@ interface IProps {
   advancePagination?: boolean;
 }
 
-const message = (totalRecordCount: any, filteredRecordCount: any) => (
-  { from, to, count }: any) =>
-  totalRecordCount === filteredRecordCount ?
-    `${from}-${to} of ${count}` :
-    filteredRecordCount === 0 ?
-      '0 records found' :
-      `${from} to ${to} of ${count} from ${totalRecordCount} records`;
+const message = (totalRecordCount: any, filteredRecordCount: any) => ({
+  from,
+  to,
+  count
+}: any) =>
+  totalRecordCount === filteredRecordCount
+    ? `${from}-${to} of ${count}`
+    : filteredRecordCount === 0
+      ? '0 records found'
+      : `${from} to ${to} of ${count} from ${totalRecordCount} records`;
 
-const Paginator: React.SFC<IProps> = ({ rowsPerPageOptions, advancePagination }) => (
+const Paginator: React.SFC<IProps> = ({
+  rowsPerPageOptions,
+  advancePagination
+}) => (
   <DataSourceContext.Consumer>
     {({ state, actions }) => {
       const props = {
         count: state.filteredRecordCount,
-        labelDisplayedRows: message(state.totalRecordCount, state.filteredRecordCount),
-        onChangePage: (e: any, p: any) => actions.updatePage(p - 1),
-        onChangeRowsPerPage: (e: any) => actions.updateItemPerPage(Number(e.target.value)),
+        labelDisplayedRows: message(
+          state.totalRecordCount,
+          state.filteredRecordCount
+        ),
+        onChangePage: (e: any, p: any) => actions.updatePage(p),
+        onChangeRowsPerPage: (e: any) =>
+          actions.updateItemPerPage(Number(e.target.value)),
         page: state.page,
         rowsPerPage: state.itemsPerPage,
         rowsPerPageOptions: rowsPerPageOptions || [10, 20, 50]
