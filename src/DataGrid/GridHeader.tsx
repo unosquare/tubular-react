3c49c3c499bd5ea1ce0baeee1583f9711c9809d4
Geo Perez <geovanni.perez@gmail.com>
2018-11-15 12:54:05 -0600

    Minor change

--- src/DataGrid/GridHeader.tsx
+++ src/DataGrid/GridHeader.tsx
@@ -18,67 +18,68 @@ const arrowStyle = {
   width: '15px'
 };
 
-const GridHeader: React.SFC = () => {
-  return (
-    <DataSourceContext.Consumer>
-      {({ actions, state }) => (
-        <TableRow>
-          {state.activeColumn && <DialogModal />}
-          {state.columns
-            .filter((col: any) => col.Visible)
-            .map((column: any) => {
-              const render = column.Sortable ? (
-                <Tooltip
-                  title='Click to sort. Press Ctrl to sort by multiple columns'
-                  placement='bottom-start'
-                  enterDelay={300}
-                >
-                  <TableSortLabel
-                    onClick={() => actions.sortColumn(column.Name)}
-                  >
-                    {column.Label}
-                    {column.SortDirection === ColumnSortDirection.ASCENDING ? (
-                      <ArrowUpward style={arrowStyle} />
-                    ) : column.SortDirection ===
-                      ColumnSortDirection.DESCENDING ? (
-                      <ArrowDownward style={arrowStyle} />
-                    ) : (
-                      <div style={arrowStyle} />
-                    )}
-                  </TableSortLabel>
-                </Tooltip>
-              ) : (
-                column.Label
-              );
-              const filter = column.Filterable && (
-                <IconButton
-                  id={column.Name}
-                  onClick={(e) => actions.setActiveColumn(column, e)}
-                >
-                  <FilterList
-                    color={
-                      column.Filter.HasFilter &&
-                      column.Filter.Operator !== CompareOperators.NONE
-                        ? 'action'
-                        : 'disabled'
-                    }
-                  />
-                </IconButton>
-              );
+const renderColumnHeader = (actions: any) => (column: any) => {
+  const render = column.Sortable ? (
+    <Tooltip
+      title='Click to sort. Press Ctrl to sort by multiple columns'
+      placement='bottom-start'
+      enterDelay={300}
+    >
+      <TableSortLabel
+        onClick={() => actions.sortColumn(column.Name)}
+      >
+        {column.Label}
+        {column.SortDirection === ColumnSortDirection.ASCENDING ? (
+          <ArrowUpward style={arrowStyle} />
+        ) : column.SortDirection ===
+          ColumnSortDirection.DESCENDING ? (
+              <ArrowDownward style={arrowStyle} />
+            ) : (
+              <div style={arrowStyle} />
+            )}
+      </TableSortLabel>
+    </Tooltip>
+  ) : (
+      column.Label
+    );
+  const filter = column.Filterable && (
+    <IconButton
+      id={column.Name}
+      onClick={(e) => actions.setActiveColumn(column, e)}
+    >
+      <FilterList
+        color={
+          column.Filter.HasFilter &&
+            column.Filter.Operator !== CompareOperators.NONE
+            ? 'action'
+            : 'disabled'
+        }
+      />
+    </IconButton>
+  );
 
-              return (
-                <TableCell
-                  key={column.Label}
-                  padding={column.Label === '' ? 'none' : 'default'}
-                >
-                  {render}
-                  {filter}
-                </TableCell>
-              );
-            })}
-        </TableRow>
-      )}
-    </DataSourceContext.Consumer>
+  return (
+    <TableCell
+      key={column.Label}
+      padding={column.Label === '' ? 'none' : 'default'}
+    >
+      {render}
+      {filter}
+    </TableCell>
   );
 };
+
+const GridHeader: React.SFC = () => (
+  <DataSourceContext.Consumer>
+    {({ actions, state }) => (
+      <TableRow>
+        {state.activeColumn && <DialogModal />}
+        {state.columns
+          .filter((col: any) => col.Visible)
+          .map(renderColumnHeader(actions))}
+      </TableRow>
+    )}
+  </DataSourceContext.Consumer>
+);
+
 export default GridHeader;
