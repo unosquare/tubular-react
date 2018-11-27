fbc612510bf5f38a6f42b03a28991fc5079b86d3
Geo Perez <geovanni.perez@gmail.com>
2018-11-27 12:48:01 -0600

    Bump version and fix lint

--- src/Models/ToolbarOptions.ts
+++ src/Models/ToolbarOptions.ts
@@ -5,18 +5,19 @@ export default class ToolbarOptions {
     public printButton: boolean;
     public searchText: boolean;
     public topPager: boolean;
-    public rowsPerPageOptions: number [];
+    public rowsPerPageOptions: number[];
     public itemsPerPage: number;
 
     constructor(options?: any) {
-       // TODO: Apply options
-       this.advancePagination = options && 'advancePagination' in options? options.advancePagination : true;
-       this.bottomPager = options && 'bottomPager' in options? options.bottomPager : true;
-       this.exportButton = options && 'exportButton' in options? options.exportButton : true;
-       this.printButton = options && 'printButton' in options? options.printButton : true;
-       this.searchText = options && 'searchText' in options? options.searchText : true;
-       this.topPager = options && 'topPager' in options? options.topPager : true;
-       this.rowsPerPageOptions = options && 'rowsPerPageOptions' in options? options.rowsPerPageOptions : [10, 20, 50, 100];
-       this.itemsPerPage = options && 'itemsPerPage' in options? options.itemsPerPage : 10;
+        this.advancePagination = options && 'advancePagination' in options ? options.advancePagination : true;
+        this.bottomPager = options && 'bottomPager' in options ? options.bottomPager : true;
+        this.exportButton = options && 'exportButton' in options ? options.exportButton : true;
+        this.printButton = options && 'printButton' in options ? options.printButton : true;
+        this.searchText = options && 'searchText' in options ? options.searchText : true;
+        this.topPager = options && 'topPager' in options ? options.topPager : true;
+        this.rowsPerPageOptions = options && 'rowsPerPageOptions' in options
+            ? options.rowsPerPageOptions
+            : [10, 20, 50, 100];
+        this.itemsPerPage = options && 'itemsPerPage' in options ? options.itemsPerPage : 10;
     }
 }
