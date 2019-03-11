9a3ccdbb55be50aca9df9eb0d9d99fbba7083548
Ana Atayde <anyx.leo@gmail.com>
2019-03-11 16:09:58 -0600

    tooltip add (#294)
    * tooltip add
    
    * Update DataGridWithRemoteDataSource.tsx

--- src/Implementations/DataGridWithRemoteDataSource.tsx
+++ src/Implementations/DataGridWithRemoteDataSource.tsx
@@ -2,6 +2,7 @@ import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
 import DataGrid from '../DataGrid/DataGrid';
 
+import Tooltip from '@material-ui/core/Tooltip';
 import * as React from 'react';
 
 /**
@@ -25,7 +26,8 @@ interface IDataGridWithRemoteDataSource {
     openModalOnClick?: boolean;
     addIcon?: boolean;
     onClick?(ev: any): void;
-  }
+    addLabel?: string;
+}
 
 const DataGridWithRemoteDataSource: React.FunctionComponent<IDataGridWithRemoteDataSource> = (props) => {
     const [modalOpen, setModalOpen] = React.useState(false);
@@ -63,9 +65,11 @@ const DataGridWithRemoteDataSource: React.FunctionComponent<IDataGridWithRemoteD
         <React.Fragment>
             <DataGrid onRowClick={onRowClick}>
                 {props.addIcon &&
-                <IconButton onClick={onAdd}>
-                    <AddIcon />
-                </IconButton>
+                    <Tooltip title={props.addLabel || 'Add'}>
+                        <IconButton onClick={onAdd}>
+                            <AddIcon />
+                        </IconButton>
+                    </Tooltip>
                 }
             </DataGrid>
             {childrenWithExtraProp}
