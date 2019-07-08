98c6978874e9c1cab9717b35aefca534415e0132
AnyxLeo <anyx.leo@gmail.com>
2019-07-08 16:49:56 -0500

    Responsive DataGrid

--- src/DataGrid/DataGrid.tsx
+++ src/DataGrid/DataGrid.tsx
@@ -1,20 +1,31 @@
+import GridList from '@material-ui/core/GridList';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Paper from '@material-ui/core/Paper';
 import makeStyles from '@material-ui/styles/makeStyles';
 import * as React from 'react';
 import { ColumnModel } from 'tubular-common';
+import { FixedLinearProgress } from 'uno-material-ui';
+import { useResolutionSwitch } from 'uno-react';
 
 import { IDataGridStorage } from '../DataGridInterfaces';
 import { DataSourceContext } from '../DataSource';
-import { DataGridTable, GridToolbar } from './';
+import ToolbarOptions from '../Models/ToolbarOptions';
+import {  DataGridCard, DataGridTable, GridToolbar } from './';
 import { DataGridProvider, IDataGridContext } from './DataGridContext';
+import { Paginator } from './Paginator';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: any) => ({
+  mobileSpacer: {
+    flexShrink: '1',
+  },
   root: {
     overflowX: 'auto',
     width: '100%',
   },
-});
+}));
+
+const outerWidth = 800;
+const timeout = 1000;
 
 interface IProps extends IDataGridContext {
   storage?: IDataGridStorage;
@@ -35,6 +46,53 @@ const DataGrid: React.FunctionComponent<IProps> = (props, {
 }) => {
   const classes = useStyles(props);
   const { state } = React.useContext(DataSourceContext);
+  const [isMobileResolution] = useResolutionSwitch(outerWidth, timeout);
+
+  if (isMobileResolution) {
+    const toolbarGridOptions = new ToolbarOptions({
+      advancePagination: false,
+      bottomPager: false,
+      exportButton: false,
+      printButton: false,
+      rowsPerPageOptions: [5, 10],
+      topPager: false,
+    });
+
+    return (
+      <DataGridProvider
+        toolbarOptions={toolbarGridOptions}
+        gridName={gridName}
+        storage={storage}
+      >
+        <Paper className={classes.root}>
+          <GridToolbar>
+            {children}
+          </GridToolbar>
+          <FixedLinearProgress isLoading={state.isLoading} />
+          <GridList
+            cellHeight='auto'
+            cols={1}
+            className={classes.mobileGrid}
+          >
+            {
+              state.data.map((row: any, index: any) =>
+                (
+                  <DataGridCard
+                    columns={state.columns}
+                    item={row}
+                    onClickCallback={onRowClick}
+                    key={index}
+                  />
+                ))
+            }
+          </GridList>
+          <div className={classes.mobileSpacer}>
+          <Paginator />
+          </div>
+        </Paper>
+      </DataGridProvider>
+    );
+  }
 
   return (
     <DataGridProvider
@@ -46,9 +104,7 @@ const DataGrid: React.FunctionComponent<IProps> = (props, {
         <GridToolbar>
           {children}
         </GridToolbar>
-        <div>
-          {state.isLoading && <LinearProgress />}
-        </div>
+        <FixedLinearProgress isLoading={state.isLoading} />
         <DataGridTable
           bodyRenderer={bodyRenderer}
           footerRenderer={footerRenderer}
---
+++ src/DataGrid/DataGridCard.tsx
@@ -0,0 +1,95 @@
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/styles/makeStyles';
+import * as React from 'react';
+import { ColumnDataType } from 'tubular-common';
+import { humanize } from 'uno-react';
+
+const useStyles = makeStyles(({ palette }: any) => ({
+    cardActions: {
+        justifyContent: 'flex-end',
+        paddingTop: 0,
+    },
+    cardBtn: {
+        color: palette.primary.dark,
+        textDecoration: 'none',
+    },
+    cardMobile: {
+        marginBottom: '2px',
+        marginLeft: '10px',
+        marginRight: '10px',
+        maxHeight: '400px',
+        minHeight: '200px',
+        minWidth: '95%',
+    },
+    dataLabel: {
+        flexDirection: 'column',
+        flexGrow: 1,
+        fontWeight: 'bold',
+        marginBottom: '3px',
+        marginLeft: '2px',
+        marginRight: '4px',
+        marginTop: '2px',
+        textAlign: 'right',
+        width: '50%',
+    },
+    dataRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start ',
+    },
+    dataValue: {
+        flexDirection: 'column',
+        flexGrow: 1,
+        margin: '2px',
+        textAlign: 'left',
+        width: '50%',
+    },
+}));
+
+export const DataGridCard = ({ columns, item, onClickCallback }) => {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.cardMobile}>
+            <CardContent>
+                {
+                    columns.map((column: any, index: any) => (
+                    <div className={classes.dataRow} key={index}>
+                        <div className={classes.dataLabel}>
+                            {humanize(column.Name)}:
+                        </div>
+                        <div className={classes.dataValue}>
+                        {
+                            column.DataType === ColumnDataType.BOOLEAN ?
+                            <input type='checkbox' checked={item[column.Name]} disabled={true}/>
+                            : column.DataType === ColumnDataType.STRING ?
+                            item[column.Name].length > 50 ? item[column.Name].substring(0, 50) + '...'
+                            : item[column.Name]
+                            : item[column.Name]
+                        }
+                    </div>
+                    </div>
+                    ))
+                }
+                <CardActions className={classes.cardActions}>
+                    {onClickCallback &&
+                        <Button
+                            size='small'
+                            onClick={onClickCallback}
+                            className={classes.cardBtn}
+                        >
+                            <b>Edit</b>
+                        </Button>
+                    }
+                </CardActions>
+            </CardContent>
+        </Card>
+    );
+
+};
