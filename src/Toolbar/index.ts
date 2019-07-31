783af2211b9c31deeacff37b7ba26dd148f05d47
Alejandro Ocampo <kadosh.ivan@gmail.com>
2019-07-31 12:12:20 -0500

    Added Toolbar package to exports on index.ts

--- src/Toolbar/index.ts
+++ src/Toolbar/index.ts
@@ -1,7 +1,6 @@
 import { GridToolbar } from './GridToolbar';
 import { ToolbarOptions } from './ToolbarOptions';
 
-// export * from './GridToolbar';
 export {
     GridToolbar,
     ToolbarOptions,
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './Storage';
 export * from './DataGridInterfaces';
 export * from './Pagination';
 export * from './Filtering';
+export * from './Toolbar';
