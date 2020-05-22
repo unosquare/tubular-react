13b5ab177fb4424c9a0401db9f866eefb9d3db57
Alejandro Ocampo <kadosh.ivan@gmail.com>
2020-05-22 11:58:02 -0500

    Fixing Drawer content when several filters

--- src/DataGrid/FeaturesDrawer.tsx
+++ src/DataGrid/FeaturesDrawer.tsx
@@ -5,22 +5,41 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Drawer from '@material-ui/core/Drawer';
 import { FiltersContainer } from '../Filtering/FiltersContainer';
-import PrintIcon from '@material-ui/icons/Print';
-import ExportIcon from '@material-ui/icons/ImportExport';
 import FilterListIcon from '@material-ui/icons/FilterList';
-import ToggleColumnsIcon from '@material-ui/icons/ViewWeek';
 
 import { ColumnModel, CompareOperators, ColumnDataType } from 'tubular-common';
-import { Button, Grid, IconButton, AppBar } from '@material-ui/core';
+import { Button, Grid, AppBar, makeStyles } from '@material-ui/core';
 
 interface TabPanelProps {
     children?: React.ReactNode;
-    index: any;
-    value: any;
+    index: string;
+    value: string;
 }
+
+const useStyles = makeStyles({
+    tabPanel: {
+        // It seems the appbar for tabs will always be 72px
+        height: 'calc(100% - 72px)',
+        overflow: 'auto',
+    },
+    mainWrapper: {
+        height: '100%',
+        width: 400,
+        padding: 20,
+        position: 'relative',
+        overflow: 'hidden',
+    },
+    featureContainer: {
+        overflow: 'hidden',
+    },
+    actionsArea: {
+        paddingTop: 20,
+    },
+});
 
 const TabPanel = (props: TabPanelProps) => {
     const { children, value, index, ...other } = props;
+    const classes = useStyles();
 
     return (
         <Typography
@@ -29,6 +48,7 @@ const TabPanel = (props: TabPanelProps) => {
             hidden={value !== index}
             id={`wrapped-tabpanel-${index}`}
             aria-labelledby={`wrapped-tab-${index}`}
+            className={classes.tabPanel}
             {...other}
         >
             <Box p={3}>{children}</Box>
@@ -61,11 +81,12 @@ export const FeaturesDrawer: React.FunctionComponent<FeaturesDrawerProps> = ({
     onApplyFeatures,
     togglePanel,
 }: FeaturesDrawerProps) => {
-    const [tempColumns, setTempColumns] = React.useState(copyColumns(columns));
+    const tempColumns = copyColumns(columns);
+    const classes = useStyles();
 
     const [value, setValue] = React.useState('filters');
 
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    const handleChange = (_event: React.ChangeEvent<{}>, newValue: string) => {
         setValue(newValue);
     };
 
@@ -76,35 +97,38 @@ export const FeaturesDrawer: React.FunctionComponent<FeaturesDrawerProps> = ({
 
     return (
         <Drawer anchor="right" open={true}>
-            <div role="presentation" style={{ height: '100%', width: '375px', padding: 20 }}>
-                <Grid container={true} direction="column" justify="space-between" style={{ height: '100%' }}>
-                    <Grid item={true}>
-                        <AppBar position="static">
-                            <Tabs value={value} onChange={handleChange} aria-label="wrapped label tabs example">
-                                <Tab value="filters" icon={<FilterListIcon />} label="Filters" />
-                                <Tab value="toggleColumns" icon={<ToggleColumnsIcon />} label="Columns" />
-                            </Tabs>
-                        </AppBar>
-                        <TabPanel value={value} index="filters">
-                            <FiltersContainer
-                                columns={tempColumns.filter((c) => c.filterable)}
-                                onApply={onApplyClick}
-                            />
-                        </TabPanel>
-                        <TabPanel value={value} index="toggleColumns">
+            <Grid
+                role="presentation"
+                className={classes.mainWrapper}
+                container={true}
+                direction="column"
+                justify="space-between"
+                wrap="nowrap"
+                style={{ height: '100%' }}
+            >
+                <Grid item={true} className={classes.featureContainer}>
+                    <AppBar position="static">
+                        <Tabs value={value} onChange={handleChange} aria-label="wrapped label tabs example">
+                            <Tab value="filters" icon={<FilterListIcon />} label="Filters" />
+                            {/* <Tab value="toggleColumns" icon={<ToggleColumnsIcon />} label="Columns" /> */}
+                        </Tabs>
+                    </AppBar>
+                    <TabPanel value={value} index="filters">
+                        <FiltersContainer columns={tempColumns.filter((c) => c.filterable)} onApply={onApplyClick} />
+                    </TabPanel>
+                    {/* <TabPanel value={value} index="toggleColumns">
                             Toggle Columns
-                        </TabPanel>
-                    </Grid>
-                    <Grid container={true} item={true} direction="row">
-                        <Button variant="contained" color="primary" onClick={onApplyClick}>
-                            Apply
-                        </Button>
-                        <Button variant="outlined" color="secondary" onClick={togglePanel} style={{ marginLeft: 10 }}>
-                            Cancel
-                        </Button>
-                    </Grid>
+                        </TabPanel> */}
                 </Grid>
-            </div>
+                <Grid container={true} className={classes.actionsArea} item={true} direction="row">
+                    <Button variant="contained" color="primary" onClick={onApplyClick}>
+                        Apply
+                    </Button>
+                    <Button variant="outlined" color="secondary" onClick={togglePanel} style={{ marginLeft: 10 }}>
+                        Cancel
+                    </Button>
+                </Grid>
+            </Grid>
         </Drawer>
     );
 };
